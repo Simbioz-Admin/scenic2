f93944fbbceefd89e3221be475fe22bac7bef413
Chesnel <pacman@poseidon.(none)>
2013-04-22 18:45:03 -0400

    add private message (receive not include for the moment)

--- js/views/irc.js
+++ js/views/irc.js
@@ -6,18 +6,21 @@ define([
 
 		var ircView = Backbone.View.extend({
 			el : '#chat',
+			userPrivate : null,
 			events : {
 				"click #submit-chat" : "connect",
-				"keypress #value-input-msg" : "send_msg"
+				"keypress #value-input-msg" : "send_msg",
+				"click .user" : "selectPrivateUser",
+				"click #remove-userPrivate" : "removePrivateUser"
 
 			},
 			initialize : function()
 			{
 				console.log("init chat-irc");
 				var that = this;
-				socket.on("receiveMsg-irc", function(user, msg)
+				socket.on("receiveMsg-irc", function(user, msg, type)
 				{	
-					that.add_msg(user, msg);
+					that.add_msg(user, msg, type);
 				});
 			},
 			connect : function()
@@ -41,19 +44,47 @@ define([
 				if(event.which == 13) {
 					var msg = $("#value-input-msg").val();
 					$("#value-input-msg").val("");
-					socket.emit("sendMsg-irc", msg);
-					this.add_msg($("#your-user").html(), msg);
+
+					if(this.userPrivate)
+					{
+						socket.emit("sendMsgPrivate-irc", this.userPrivate, msg);
+						this.add_msg($("#your-user").html(), msg, this.userPrivate);
+					}
+					else
+					{
+						console.log("SEND : ", msg);
+						socket.emit("sendMsg-irc", msg);
+						this.add_msg($("#your-user").html(), msg, "public");
+					}
+
+					
 
 				}
 			},
-			add_msg : function(user, msg)
+			add_msg : function(user, msg, userPrivate)
 			{
-				var message = _.template(TemplateMsg, {user : user, msg : msg});
+				var message = _.template(TemplateMsg, {user : user, msg : msg, type : userPrivate});
 				$("#content-chat").append(message);
 
 				var objDiv = document.getElementById("content-chat");
 				objDiv.scrollTop = objDiv.scrollHeight;
 
+			},
+			selectPrivateUser : function(){
+
+				$("#userPrivate").remove();
+				this.userPrivate = $(event.target).data("username");
+
+				$("#value-input-msg").after('<div id="userPrivate"><div id="remove-userPrivate">x</div>'+this.userPrivate+'</div>')
+										
+				var sizeSticky = $("#userPrivate").outerWidth()+5;
+				$("#value-input-msg").css({paddingLeft : sizeSticky, width : $("#value-input-msg").width()-sizeSticky+5 });
+			},
+			removePrivateUser : function(event)
+			{
+				this.userPrivate = null;
+				$(event.target).parent().remove();
+				$("#value-input-msg").removeAttr("style");
 			}
 
 		});

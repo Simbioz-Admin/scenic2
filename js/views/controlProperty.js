60d3623148c4e1ad2b8c700382d591f5a6bf801a
Chesnel <pacman@poseidon.(none)>
2013-09-10 15:09:50 -0400

    commit missing files

---
+++ js/views/controlProperty.js
@@ -0,0 +1,46 @@
+define([
+	'underscore',
+	'backbone',
+	'text!/templates/destination.html',
+], function(_, Backbone, TemplateDestination) {
+
+	var ViewDestination = Backbone.View.extend({
+		tagName: 'td',
+		className: 'nameInOut',
+		table: null,
+		events: {
+			"click .edit": "edit",
+			"click .remove": "removeClick"
+		},
+		initialize: function() {
+			this.model.on('remove', this.removeView, this);
+			this.table = this.options.table;
+
+			var that = this,
+				template = _.template(TemplateDestination, {
+					name: this.model.get("name")
+				});
+
+			console.log(this.model);
+			$(this.el).append(template);
+			$("#" + this.table + " .destinations").append($(this.el));
+
+			$("#control .property").each(function(index, source) {
+				console.log(index);
+				$(this).append("<td class='box connect-properties' data-nameandproperty='" + that.model.get("name") + "'></td>");
+			});
+
+		},
+		edit: function() {
+			this.model.edit();
+		},
+		removeClick: function() {
+			this.model.delete();
+		},
+		removeView: function() {
+			this.remove();
+		}
+	});
+
+	return ViewDestination;
+})

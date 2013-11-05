af156146dd5bbc8ecfddd7e0c0d55fea729eaeda
Chesnel <pacman@poseidon.(none)>
2013-11-05 12:03:31 -0500

    add documentation

--- js/collections/tables.js
+++ js/collections/tables.js
@@ -1,53 +1,79 @@
-define([
-	'underscore',
-	'backbone',
-	'models/table'
-], function(_, Backbone, ModelTable) {
+define(
 
-	var CollectionTables = Backbone.Collection.extend({
-		model: ModelTable,
-		currentTable: config.defaultPanelTable,
-		parse: function(results, xhr) {
-			return results;
-		},
-		initialize: function() {
-			console.log("init CollectionTable");
-			//create default tables 
-			//create the first table for manage transfert
+	/** 
+     *	Manage the collection of table. By default we have table controler and transfer
+     *	@exports collections/tables
+ 	 */
 
-			this.add({
-				name: "controler",
-				type: "control",
-				description: "it's the default table for controler.",
-				menus: {
-					sources: {
-						type: "midi",
-						name: "control midi",
-						dataName: "midisrc"
-					},
-					destinations: {
-						type: "quiddsProperties",
-						name: "property"
+	[
+		'underscore',
+		'backbone',
+		'models/table'
+	],
+	function(_, Backbone, ModelTable){
+
+		/** 
+		 *	@constructor
+		 *  @requires Underscore
+		 *  @requires Backbone
+		 *	@requires ModelTable
+		 *  @augments module:Backbone.Collection
+		 */
+
+		var CollectionTables = Backbone.Collection.extend(
+
+		/**
+		 *	@lends module:collections/tables~CollectionTables.prototype
+		 */
+
+		{
+			model: ModelTable,
+			currentTable: config.defaultPanelTable,
+
+
+			/** Execute when the collection is initialized
+			 *	We declare all events for receive information about quiddities
+			 */
+
+			initialize: function() {
+
+				/* Default table controler. Control the properties values with device (midi osc, etc..) */
+				this.add({
+					name: "controler",
+					type: "control",
+					description: "it's the default table for controler.",
+					menus: {
+						sources: {
+							type: "midi",
+							name: "control midi",
+							dataName: "midisrc"
+						},
+						destinations: {
+							type: "quiddsProperties",
+							name: "property"
+						}
 					}
-				}
-			});
-			this.add({
-				name: "transfer",
-				type: "transfer",
-				description: "it's the default table for transfer.",
-				menus: {
-					sources: {
-						type: "quidds",
-						name: "source"
-					},
-					destinations: {
-						type: "client",
-						name: "destination"
-					}
-				}
-			});
-		}
-	});
+				});
 
-	return CollectionTables;
-})
+
+				/* This table manage the transfer between source and destination */
+				this.add({
+					name: "transfer",
+					type: "transfer",
+					description: "it's the default table for transfer.",
+					menus: {
+						sources: {
+							type: "quidds",
+							name: "source"
+						},
+						destinations: {
+							type: "client",
+							name: "destination"
+						}
+					}
+				});
+			}
+		});
+
+		return CollectionTables;
+	})

659556e327d86e7a16b0e1dbca085012681bf639
Michal Seta <mis@artengine.ca>
2014-03-20 13:54:11 -0400

    handle the installation from server script as well

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+var fs = require('fs')
+if (!fs.existsSync(process.env.HOME + "/.scenic2")) {
+    console.log("~/.scenic2 does not exist, running the installer")
+    var scenic = require('child_process').spawn
+    var installer = scenic(__dirname + "/scenic2-installer")
+    installer.on('close', function(){
+        scenic("notify-send", ["Scenic2","Components installed. You may now run scenic"])
+        return
+    })
+}
+
 var config = require('./scenic/config.js'),
         argv = require('./scenic/arguments.js')(config),
         express = require("express"),

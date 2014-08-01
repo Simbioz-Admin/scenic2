9cb1890622256078c05b0eb3602edb80de319e8e
Michal Seta <mis@artengine.ca>
2014-08-01 19:01:59 -0400

    CPU monitor done and beginning net

--- js/views/systemusage/sysmon.js
+++ js/views/systemusage/sysmon.js
@@ -36,6 +36,7 @@ define(
 
                 /* Called when the view ConnexionView is initialized */
                 initialize: function() {
+                    console.log("d3 version ", d3.version);
                     console.log("sysmon loaded!");
                     console.log("initialize this is ", this);
                     var _this = this;
@@ -52,6 +53,37 @@ define(
                     $("#header").append(template);
                     //$("body").html(this.el);
                     //$(this.el).html(template);
+
+                    // FIXME: style these with CSS
+                    var cpuBg = d3.select(".chart")
+                        .append("rect")
+                        .attr("class", "monBg")
+                        .attr("width", 50)
+                        .attr("height", 50);
+
+                    var cpuLabel = d3.select(".chart")
+                        .append("text")
+                        .attr("class", "monLabel")
+                        .attr("x", 5)
+                        .attr("y", 15)
+                        //.attr("fill", "grey")
+                        .text("CPU");
+
+                    var netBg = d3.select(".chart")
+                        .append("rect")
+                        .attr("x", 51)
+                        .attr("class", "monBg")
+                        .attr("width", 50)
+                        .attr("height", 50);
+
+                    var netLabel = d3.select(".chart")
+                        .append("text")
+                        .attr("class", "monLabel")
+                        .attr("x", 55)
+                        .attr("y", 15)
+                        //.attr("fill", "grey")
+                        .text("NET");
+
                    
 
                     return this;
@@ -63,43 +95,90 @@ define(
                     //this.renderBars(info);
                     //console.log("info in render: ", info);
                     var cpus = [];
+                    var net = [];
                     for (key in info) {
-                        //console.log("key", sysusage[key])
-                        if (key == "cpu") {
+                            //console.log("key", sysusage[key])
+                            if (key == "cpu") {
                             for (cpu in info[key]) {
                                 //console.log("cpu", cpu, info[key][cpu]);
                                 cpus.push(info[key][cpu]);
                             }
-                            //console.log(cpus);
+                                this.cpuMon(cpus);
+                        } else if (key == "net") {
+                            for (eth in info[key]) {
+                                net.push(info[key][eth])
+                            }
+                            this.netMon(net);
                         }
                     }
-                    this.d3(cpus);
+                    
+                    
                     
                 },
-                d3: function (data) {
-                    var width = 80,
-                    height = 40;
+                netMon: function netMon(data) {
+                    //console.log("TODO: implement netMonitor")
+                    var width = 200;
+                    var height = 50;
+                    var barWidth = (width / 4) / data.length;
                     var y = d3.scale.linear()
                             .range([height, 0]);
                     var chart = d3.select(".chart")
-                        .attr("x", 600)
                         .attr("width", width)
                         .attr("height", height);
-                    var bar = chart.selectAll("g")
-                        .data(data)
-                        .enter().append("g")
-                        .attr("transform", function(d, i){
-                            return "translate(" + i * 10 + ", 0)";
-                        });
-                    bar.append("rect")
-                        .attr("y", function (d) {return y(d.user)})
+
+                    var bar = chart.selectAll(".bar")
+                        .data(data);
+                    bar.enter()
+                        .append("rect")
+                        .attr("class", "bar");
+
+                    bar.transition(0.1)
+                        .attr("y", function (d) {return y(d.tx_rate)})
                         .attr("height", function (d, i) { 
-                            console.log("bar " + i + " should be " + d.user);
-                            return height - y(d.user); 
+                            return height - y(d.tx_rate); 
                         })
-                        .attr("width", 5);
+                        .attr("width", barWidth - 1)
+                        .attr("transform", function(d, i){
+                            return "translate(" + 50 + (i * barWidth) + ", 0)";
+                        });
+
+                    function type(d) {
+                        d.value = +d.value; // coerce to number
+                        return d;
+                    }
                     
-                }
+                },
+                cpuMon: function (data) {
+                    var width = 200;
+                    var height = 50;
+                    var barWidth = (width / 4) / data.length;
+                    var y = d3.scale.linear()
+                            .range([height, 0]);
+                    var chart = d3.select(".chart")
+                        .attr("width", width)
+                        .attr("height", height);
+
+                    var bar = chart.selectAll(".bar")
+                        .data(data);
+                    bar.enter()
+                        .append("rect")
+                        .attr("class", "bar");
+
+                    bar.transition(0.1)
+                        .attr("y", function (d) {return y(d.total)})
+                        .attr("height", function (d, i) { 
+                            return height - y(d.total); 
+                        })
+                        .attr("width", barWidth - 1)
+                        .attr("transform", function(d, i){
+                            return "translate(" + i * barWidth + ", 0)";
+                        });
+
+                    function type(d) {
+                        d.value = +d.value; // coerce to number
+                        return d;
+                    }
+                },
             });
 
         return SysmonView;

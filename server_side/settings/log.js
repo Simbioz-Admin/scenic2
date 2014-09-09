f72f8d60f9d4ff459b04c9eb71dbd53c7a2f1c39
Michal Seta <mis@artengine.ca>
2014-09-09 19:02:06 -0400

    handle installation path correctly for express and logging

--- server_side/settings/log.js
+++ server_side/settings/log.js
@@ -1,88 +1,89 @@
 define(
-    ['winston', 'config'],
+  ['winston', 'config'],
 
-    function(winston, config) {
+  function(winston, config) {
 
-        var customLevels = {
-            levels: {
-                switcher: 0,
-                debug: 1,
-                info: 2,
-                warn: 3,
-                error: 4
-            },
-            colors: {
-                switcher: 'blue',
-                debug: 'white',
-                info: 'green',
-                warn: 'orange',
-                error: 'red'
-            }
-        };
+    var customLevels = {
+      levels: {
+        switcher: 0,
+        debug: 1,
+        info: 2,
+        warn: 3,
+        error: 4
+      },
+      colors: {
+        switcher: 'blue',
+        debug: 'white',
+        info: 'green',
+        warn: 'orange',
+        error: 'red'
+      }
+    };
 
-        /* check if folder logs exist */
-        var fs = require('fs');
+    /* check if folder logs exist */
+    var fs = require('fs');
 
-        if (!fs.existsSync(config.pathLogs)) {
-            // create folder
-            fs.mkdir(config.pathLogs, function(e) {
-                if (e) return console.log(e);
-            })
-        }
-
-
-        winston.addColors(customLevels.colors);
-        var log = new(winston.Logger)({
-            levels: customLevels.levels,
-            transports: [
-                new(winston.transports.Console)({
-                    'colorize': true,
-                    level: config.logLevel
-                }),
-                new(winston.transports.File)({
-                    filename: 'logs/logging-file.log'
-                })
-            ]
-        });
-
-
-        var logger_info_old = log.error;
-        log.error = function(msg) {
-            var fileAndLine = traceCaller(1);
-            return logger_info_old.call(this, fileAndLine + ": " + msg);
-        }
-
-        log.debug("Init log.js");
-        /**
-         * examines the call stack and returns a string indicating
-         * the file and line number of the n'th previous ancestor call.
-         * this works in chrome, and should work in nodejs as well.
-         *
-         * @param n : int (default: n=1) - the number of calls to trace up the
-         *   stack from the current call.  `n=0` gives you your current file/line.
-         *  `n=1` gives the file/line that called you.
-         */
-
-        function traceCaller(n) {
-            if (isNaN(n) || n < 0) n = 1;
-            n += 1;
-            var s = (new Error()).stack,
-                a = s.indexOf('\n', 5);
-            while (n--) {
-                a = s.indexOf('\n', a + 1);
-                if (a < 0) {
-                    a = s.lastIndexOf('\n', s.length);
-                    break;
-                }
-            }
-            b = s.indexOf('\n', a + 1);
-            if (b < 0) b = s.length;
-            a = Math.max(s.lastIndexOf(' ', b), s.lastIndexOf('/', b));
-            b = s.lastIndexOf(':', b);
-            s = s.substring(a + 1, b);
-            return s;
-        }
-
-        return log;
+    if (!fs.existsSync(config.pathLogs)) {
+      // create folder
+      fs.mkdir(config.pathLogs, function(e) {
+        if (e) return console.log(e);
+      })
     }
+
+
+    winston.addColors(customLevels.colors);
+    var log = new(winston.Logger)({
+      levels: customLevels.levels,
+      transports: [
+        new(winston.transports.Console)({
+          'colorize': true,
+          level: config.logLevel
+        }),
+        new(winston.transports.File)({
+          filename: config.pathLogs + 'logging-file.log'
+          //filename: '/tmp/scenic.log'
+        })
+      ]
+    });
+
+
+    var logger_info_old = log.error;
+    log.error = function(msg) {
+      var fileAndLine = traceCaller(1);
+      return logger_info_old.call(this, fileAndLine + ": " + msg);
+    }
+
+    log.debug("Init log.js");
+    /**
+     * examines the call stack and returns a string indicating
+     * the file and line number of the n'th previous ancestor call.
+     * this works in chrome, and should work in nodejs as well.
+     *
+     * @param n : int (default: n=1) - the number of calls to trace up the
+     *   stack from the current call.  `n=0` gives you your current file/line.
+     *  `n=1` gives the file/line that called you.
+     */
+
+    function traceCaller(n) {
+      if (isNaN(n) || n < 0) n = 1;
+      n += 1;
+      var s = (new Error()).stack,
+      a = s.indexOf('\n', 5);
+      while (n--) {
+        a = s.indexOf('\n', a + 1);
+        if (a < 0) {
+          a = s.lastIndexOf('\n', s.length);
+          break;
+        }
+      }
+      b = s.indexOf('\n', a + 1);
+      if (b < 0) b = s.length;
+      a = Math.max(s.lastIndexOf(' ', b), s.lastIndexOf('/', b));
+      b = s.lastIndexOf(':', b);
+      s = s.substring(a + 1, b);
+      return s;
+    }
+
+    return log;
+  }
 );

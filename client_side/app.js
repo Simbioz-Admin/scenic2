4f6df21da78170e6924f067f01bc669131ce3bca
Michal Seta <mis@artengine.ca>
2014-09-12 12:13:39 -0400

    fix whitespace

--- client_side/app.js
+++ client_side/app.js
@@ -1,126 +1,126 @@
 // Filename: app.js
 define(
 
+  /** 
+   *	View Source
+   *	The source view is for each source type quiddity create whatsoever to control or transfer table
+   *	@exports Views/Launch
+   */
+
+  [
+    'underscore',
+    'backbone',
+    'jquery',
+    'collections/tables', 'collections/classes_doc', 'collections/receivers', 'collections/quidds', 'collections/control_properties', 'collections/loggers', 'collections/users', 'collections/channels-irc',
+    'views/destinations', 'views/global', 'views/quidds', 'views/destinationProperties', 'views/loggers', 'views/users/users', 'views/ircs', 'views/systemusage/sysmon'
+
+  ],
+
+  function(
+    _,
+    Backbone,
+    $,
+    CollectionTables, CollectionClassesDoc, CollectionReceivers, CollectionQuidds, CollectionDestinationProperties, CollectionLoggers, CollectionUsers, CollectionIrcs,
+    ViewDestinations, ViewGlobal, ViewQuidds, ViewDestinationProperties, ViewLoggers, ViewUsers, ViewIrcs, ViewSysmon
+  ) {
+
     /** 
-     *	View Source
-     *	The source view is for each source type quiddity create whatsoever to control or transfer table
-     *	@exports Views/Launch
+     * @constructor
+     * @requires Underscore
+     * @requires Jquery
+     * @requires CollectionTables
+     * @requires CollectionClassesDoc
+     * @requires CollectionReceivers
+     * @requires CollectionQuidds
+     * @requires CollectionDestinationProperties
+     * @requires CollectionLoggers
+     * @requires CollectionIrcs
+     * @requires ViewDestinations
+     * @requires ViewGlobal
+     * @requires ViewQuidds
+     * @requires ViewDestinationProperties
+     * @requires ViewLoggers
+     * @requires ViewIrcs
+     * @requires ViewSysmon 
+     *  @augments module:Backbone.View
      */
 
-    [
-        'underscore',
-        'backbone',
-        'jquery',
-        'collections/tables', 'collections/classes_doc', 'collections/receivers', 'collections/quidds', 'collections/control_properties', 'collections/loggers', 'collections/users', 'collections/channels-irc',
-        'views/destinations', 'views/global', 'views/quidds', 'views/destinationProperties', 'views/loggers', 'views/users/users', 'views/ircs', 'views/systemusage/sysmon'
+    var initialize = function() {
+      "use strict";
 
-    ],
+      $("#currentUser").html(config.nameComputer);
 
-    function(
-        _,
-        Backbone,
-        $,
-        CollectionTables, CollectionClassesDoc, CollectionReceivers, CollectionQuidds, CollectionDestinationProperties, CollectionLoggers, CollectionUsers, CollectionIrcs,
-        ViewDestinations, ViewGlobal, ViewQuidds, ViewDestinationProperties, ViewLoggers, ViewUsers, ViewIrcs, ViewSysmon
-    ) {
+      //loading the different collections
+      collections.classesDoc = new CollectionClassesDoc();
+      collections.classesDoc.fetch({
+        success: function(response) {
 
-        /** 
-         * @constructor
-         * @requires Underscore
-         * @requires Jquery
-         * @requires CollectionTables
-         * @requires CollectionClassesDoc
-         * @requires CollectionReceivers
-         * @requires CollectionQuidds
-         * @requires CollectionDestinationProperties
-         * @requires CollectionLoggers
-         * @requires CollectionIrcs
-         * @requires ViewDestinations
-         * @requires ViewGlobal
-         * @requires ViewQuidds
-         * @requires ViewDestinationProperties
-         * @requires ViewLoggers
-         * @requires ViewIrcs
-         * @requires ViewSysmon 
-         *  @augments module:Backbone.View
-         */
+          //loading views
+          views.quidds = new ViewQuidds({
+            collection: collections.quidds
+          });
 
-        var initialize = function() {
-            "use strict";
+          collections.quidds = new CollectionQuidds();
+          
+          collections.receivers = new CollectionReceivers();
+          collections.receivers.fetch();
 
-            $("#currentUser").html(config.nameComputer);
+          collections.tables = new CollectionTables();
 
-            //loading the different collections
-            collections.classesDoc = new CollectionClassesDoc();
-            collections.classesDoc.fetch({
-                success: function(response) {
-
-                    //loading views
-                    views.quidds = new ViewQuidds({
-                        collection: collections.quidds
-                    });
-
-                    collections.quidds = new CollectionQuidds();
-                    
-                    collections.receivers = new CollectionReceivers();
-                    collections.receivers.fetch();
-
-                    collections.tables = new CollectionTables();
-
-                    collections.quidds.fetch({
-                        error: function(err) {
-                            console.log("error", err);
-                        },
-                        success: function() {
-                            console.log("quidds Loaded");
+          collections.quidds.fetch({
+            error: function(err) {
+              console.log("error", err);
+            },
+            success: function() {
+              console.log("quidds Loaded");
 
 
 
-                            collections.destinationProperties = new CollectionDestinationProperties();
-                            collections.destinationProperties.fetch();
+              collections.destinationProperties = new CollectionDestinationProperties();
+              collections.destinationProperties.fetch();
 
-                            collections.loggers = new CollectionLoggers();
-                            views.logger = new ViewLoggers({
-                                collection: collections.loggers
-                            });
+              collections.loggers = new CollectionLoggers();
+              views.logger = new ViewLoggers({
+                collection: collections.loggers
+              });
 
-                            views.global = new ViewGlobal();
-                            views.sysmon = new ViewSysmon();
-                            //views.htop = new ViewHtop();
+              views.global = new ViewGlobal();
+              views.sysmon = new ViewSysmon();
+              //views.htop = new ViewHtop();
 
-                            collections.irc = new CollectionIrcs();
-                            views.ircs = new ViewIrcs();
+              collections.irc = new CollectionIrcs();
+              views.ircs = new ViewIrcs();
 
-                            views.destinationProperties = new ViewDestinationProperties({
-                                collection: collections.destinationProperties
-                            });
-
-
-                            /* generate view for manage users */
-
-                            collections.users = new CollectionUsers();
-
-                            collections.users.fetch({
-                                success: function() {
-                                    views.users = new ViewUsers({
-                                        collection: collections.users
-                                    });
-                                }
-                            });
-                        }
-                    });
+              views.destinationProperties = new ViewDestinationProperties({
+                collection: collections.destinationProperties
+              });
 
 
+              /* generate view for manage users */
 
+              collections.users = new CollectionUsers();
+
+              collections.users.fetch({
+                success: function() {
+                  views.users = new ViewUsers({
+                    collection: collections.users
+                  });
                 }
-            });
+              });
+            }
+          });
+
 
 
         }
+      });
 
 
-        return {
-            initialize: initialize
-        };
+    }
 
-    });
+
+    return {
+      initialize: initialize
+    };
+
+  });

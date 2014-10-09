a3a57dc24f955fa284b7fb64d7da878964651ae8
Michal Seta <mis@artengine.ca>
2014-10-09 14:33:11 -0400

    remove some whitespace

--- client_side/views/editDestination.js
+++ client_side/views/editDestination.js
@@ -1,78 +1,67 @@
 define(
+  /** 
+   *	View EditQuidd
+   *	Manage interaction between a specifc node and the interface for editing
+   *	all modification are detected when we press enter or switch input
+   *	@exports Views/EditQuidd
+   */
+  [
+    'underscore',
+    'backbone',
+    'text!../../templates/editDestination.html',
+  ],
 
+  function(_, Backbone, TemplateEditDestination) {
     /** 
-     *	View EditQuidd
-     *	Manage interaction between a specifc node and the interface for editing
-     *	all modification are detected when we press enter or switch input
-     *	@exports Views/EditQuidd
+     *	@constructor
+     *  @requires Underscore
+     *  @requires Backbone
+     *	@requires TemplateEditDestination
+     *  @augments module:Backbone.View
      */
 
-    [
-        'underscore',
-        'backbone',
-        'text!../../templates/editDestination.html',
-    ],
+    var ViewEditDestination = Backbone.View.extend(
+      /**
+       *	@lends module: Views/EditQuidd~ViewEditDestination.prototype
+       */
+      {
+        events: {
+          "submit #form-destination": 'save'
+        },
 
-    function(_, Backbone, TemplateEditDestination) {
+        /* Called when the view is initialized */
+        initialize: function() {
+          var that = this;
+          //generate template for receive information about destination
+          var template = _.template(TemplateEditDestination, {
+            destination: that.model
+          });
 
+          $(this.el).append(template);
 
-        /** 
-         *	@constructor
-         *  @requires Underscore
-         *  @requires Backbone
-         *	@requires TemplateEditDestination
-         *  @augments module:Backbone.View
-         */
+          $("#panelRight .content").html($(this.el));
+          views.global.openPanel();
 
+        },
+        save: function(e) {
+          var that = this;
+          var destination = {
+            name: $("#name", this.el).val(),
+            hostName: $("#hostName", this.el).val(),
+            portSoap: $("#portSoap", this.el).val(),
+            data_streams: this.model.get("data_streams")
 
-        var ViewEditDestination = Backbone.View.extend(
+          },
+          oldId = this.model.get("id");
 
-            /**
-             *	@lends module: Views/EditQuidd~ViewEditDestination.prototype
-             */
+          socket.emit("update_destination", oldId, destination, function(data) {
+            if (data.success) {
+              views.global.closePanel();
+            }
+          });
+        }
 
-            {
+      });
 
-                events: {
-                    "submit #form-destination": 'save'
-                },
-
-
-                /* Called when the view is initialized */
-
-                initialize: function() {
-
-                    var that = this;
-                    //generate template for receive information about destination
-                    var template = _.template(TemplateEditDestination, {
-                        destination: that.model
-                    });
-
-                    $(this.el).append(template);
-
-                    $("#panelRight .content").html($(this.el));
-                    views.global.openPanel();
-
-                },
-                save: function(e) {
-                    var that = this;
-                    var destination = {
-                        name: $("#name", this.el).val(),
-                        hostName: $("#hostName", this.el).val(),
-                        portSoap: $("#portSoap", this.el).val(),
-                        data_streams: this.model.get("data_streams")
-
-                    },
-                        oldId = this.model.get("id");
-
-                    socket.emit("update_destination", oldId, destination, function(data) {
-                        if (data.success) {
-                            views.global.closePanel();
-                        }
-                    });
-                }
-
-            });
-
-        return ViewEditDestination;
-    })
+    return ViewEditDestination;
+  })

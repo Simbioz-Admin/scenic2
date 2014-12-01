3ff22f74bcd285f24d2ce2a4c4c5ba5b8af32440
Chesnel <pa@chesnel.ca>
2014-12-01 13:55:10 -0500

    add missing files

---
+++ client_side/views/users/editUser.js
@@ -0,0 +1,79 @@
+define(
+  /** 
+   *	View EditQuidd
+   *	Manage interaction between a specifc node and the interface for editing
+   *	all modification are detected when we press enter or switch input
+   *	@exports Views/EditQuidd
+   */
+  [
+    'underscore',
+    'backbone',
+    'text!../../../templates/users/editUserSip.html',
+  ],
+
+  function(_, Backbone, TemplateEditDestinationRtp) {
+    /** 
+     *	@constructor
+     *  @requires Underscore
+     *  @requires Backbone
+     *	@requires TemplateEditDestinationRtp
+     *  @augments module:Backbone.View
+     */
+
+    var ViewEditDestination = Backbone.View.extend(
+      /**
+       *	@lends module: Views/EditQuidd~ViewEditDestination.prototype
+       */
+      {
+        events: {
+          "submit #form-user": 'update',
+          "click .remove" : 'removeUser'
+        },
+
+        /* Called when the view is initialized */
+        initialize: function() {
+          var that = this;
+          //generate template for receive information about destination
+          var template = _.template(TemplateEditDestinationRtp, {
+            destination: that.model
+          });
+
+          $(this.el).append(template);
+
+          $("#panelRight .content").html($(this.el));
+          views.global.openPanel();
+
+        },
+        update: function(e) {
+          var that = this;
+          var name = $('#name', this.el).val();
+          var uri = this.model.get('uri');
+          var statusText = $('#statusText').val();
+          var status = $('#status').val();
+
+          socket.emit('updateUser', uri, name, statusText, status, function(err,msg){
+            if(err) return views.global.notification('error', err);
+            views.global.notification('valid',msg);
+            views.global.closePanel();
+          });
+
+        },
+        removeUser : function(e){
+          e.preventDefault();
+          var that = this;
+           var result = views.global.confirmation("Are you sure?", function(ok) {
+             if (ok) {
+              socket.emit("removeUser", that.model.get("uri"), function(err, msg) {
+                 if (err)  return views.global.notification("error", err);
+                 views.global.notification("valid", msg);
+                 
+              });
+               //socket.emit("invoke", "defaultrtp", "remove_destination", [that.get("name")], function(ok) {});
+             }
+           });
+        }
+
+      });
+
+    return ViewEditDestination;
+  })

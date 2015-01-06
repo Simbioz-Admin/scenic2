a79653f794d292c71c2477481c4721a31a22a8f4
Chesnel <pa@chesnel.ca>
2015-01-06 14:56:56 -0500

    start translation

--- server_side/settings/i18n.js
+++ server_side/settings/i18n.js
@@ -1,23 +1,38 @@
 define(
-    [
-        'i18n',
-        'log',
-    ],
+  [
+    'i18next',
+    'log',
+  ],
 
-    function(i18n, log) {
-
-        i18n.setLocale("en");
-        log.debug("Initialization i18n", i18n.getLocale());
-        i18n.configure({
-            locales: ['en', 'fr'],
-            directory: 'locales'
-        });
+  function(i18n, log) {
 
 
-        i18n.__("patate");
+
+    i18n.init({
+      lng: "en",
+      saveMissing : true,
+      resGetPath: 'locales/__lng__/__ns__.json',
+      resSetPath: 'locales/__lng__/__ns__.json',
+      fallbackLng : 'fr',  
+      preload: ['en', 'fr'],
+      sendMissingTo : 'fallback',
+      debug: true
+    }, function(){
+       log.debug("Translation Init : ", i18n.lng());
+    });
+
+    // i18n.setLocale("en");
+    // log.debug("Initialization i18n", i18n.getLocale());
+    // i18n.configure({
+    //     locales: ['fr'],
+    //     directory: require.toUrl('.')+'/locales'
+    // });
 
 
-    }
+    // i18n.__("patate");
+
+
+  }
 
 
 );
--- server_side/switcher/quidds.js
+++ server_side/switcher/quidds.js
@@ -1,6 +1,5 @@
-define(
-  ['config', 'switcher', 'log', 'underscore', 'jquery'],
-  function(config, switcher, log, _, $) {
+define(['config', 'switcher', 'log', 'underscore', 'jquery', 'i18next'],
+  function(config, switcher, log, _, $, i18n) {
 
 
     var io;
@@ -20,14 +19,13 @@ define(
 
     function create(className, quiddName, socketId, cb) {
 
-
       if (!cb) cb = socketId; //its not always a user ask for create a quidd
 
       quiddName = (quiddName ? switcher.create(className, quiddName) : switcher.create(className));
       if (quiddName) {
 
         /* we stock id of socket and name of quidd because we want to alert the user 
-           client side when the quiddity is created for show the properties */
+        client side when the quiddity is created for show the properties */
 
         log.debug("quiddity " + quiddName + " (" + className + ") created.");
 
@@ -36,8 +34,11 @@ define(
         cb(null, quiddInfo);
 
       } else {
-        log.error("failed to create a quiddity class ", className);
-        cb("failed to create " + className + " maybe this name is already used?");
+        var erroMsg = i18n.t("failed to create __quiddName__ maybe this name is already used?", {
+          quiddName: className
+        });
+        log.error(erroMsg);
+        cb(erroMsg);
       }
 
     }
@@ -114,9 +115,12 @@ define(
       }
 
       if (quiddName && property && value) {
-        var ok = switcher.set_property_value(quiddName, property, String(value));
-        if (ok) {
-          var msgError = "failed to set the property " + property + " of " + quiddName;
+        var setProperty = switcher.set_property_value(quiddName, property, String(value));
+        if (!setProperty) {
+          var msgError = i18n.t("failed to set the property __property__ of __quiddName__", {
+            property: property,
+            quiddName: quiddName
+          });
           log.error(msgError);
           return cb(msgError);
         }
@@ -124,12 +128,11 @@ define(
         log.debug("the porperty " + property + " of " + quiddName + "is set to " + value);
         cb(null);
 
-        } else {
-          log.error("failed to set the property " + property + " of " + quiddName);
-          socket.emit("msg", "error", "the property " + property + " of " + quiddName + " is not set");
-        }
       } else {
-        log.error("missing arguments for set property value :", quiddName, property, value);
+        var msgError = i18n.t("missing arguments for set property value "),
+          quiddName, property, value;
+        log.error(msgError);
+        cb(msgError);
       }
     }
 
@@ -187,7 +190,7 @@ define(
     function get_properties_description(quiddName, cb) {
 
       var properties_description = $.parseJSON(switcher.get_properties_description(quiddName)).properties,
-      properties_to_send = {};
+        properties_to_send = {};
 
       if (properties_description && properties_description.error) {
         var msg = properties_description.error + "(quiddity : " + quiddName + ")";
@@ -207,7 +210,9 @@ define(
     function get_methods_description(quiddName, cb) {
       var methods = $.parseJSON(switcher.get_methods_description(quiddName)).methods;
       if (!methods) {
-        var msg = "failed to get methods description " + quiddName;
+        var msg = i18n.t("failed to get methods description __quiddName__", {
+          quiddName: quiddName
+        });
         cb(msg);
         log.error(msg);
         return;
@@ -222,7 +227,10 @@ define(
     function get_method_description(quiddName, method, cb) {
       var descriptionJson = $.parseJSON(switcher.get_method_description(quiddName, method));
       if (!descriptionJson) {
-        var msg = "failed to get " + method + " method description" + quiddName;
+        var msg = i18n.t("failed to get __method__ method description __quiddName", {
+          method: method,
+          quiddName: quiddName
+        });
         cb(msg, err);
         log.error(msg);
         return;
@@ -236,25 +244,19 @@ define(
       log.debug("Get property value", quiddName, property);
       if (quiddName && property) {
         try {
-          log.debug("trying to get property value")
-          // property_value = $.parseJSON(switcher.get_property_value(quiddName, property));
           var property_value = $.parseJSON(switcher.get_property_value(quiddName, property));
-          console.log("property value in try", property_value);
         } catch (e) {
-          log.error("Error", e.stack)
-          console.log(e.stack)
           var property_value = switcher.get_property_value(quiddName, property);
-          console.log("property_value in catch", property_value);
-          log.debug("error getting property value", e);
         }
-        log.debug("getting property value seems successfull", property_value);
       } else {
-        var msg = "failed o get property value (quiddity: " + quiddName + " property: " + property;
+        var msg = i18n.t("failed to get property value (quiddity: __quiddName__ property: __property__", {
+          quiddName: quiddName,
+          property: property
+        });
         cb(msg);
         log.error(msg);
         return;
       }
-      log.debug("got this property value: ", property_value);
       cb(null, property_value);
     }
 
@@ -263,11 +265,14 @@ define(
       var invoke = switcher.invoke(quiddName, method, parameters);
       log.debug("the method " + method + " of " + quiddName + " is invoked with " + parameters);
       if (!invoke) {
-        var msgError = "failed to invoke " + quiddName + " method " + method;
+        var msgError = i18n.t("failed to invoke __quiddName__ method __method__", {
+          quiddName: quiddName,
+          method: method
+        });
         log.error(msgError);
-        return cb(msgError);        
+        return cb(msgError);
       }
-      if(cb) cb(null, invoke);
+      if (cb) cb(null, invoke);
 
       if (method == "remove_udp_stream_to_dest")
         io.sockets.emit("remove_connection", invoke, quiddName, parameters);
